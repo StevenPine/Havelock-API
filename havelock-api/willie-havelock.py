08cd0ee027a71f737d1b7319fd0036c99d572e50
Baldur <baldur@Asgard.meow>
2014-08-06 13:32:07 -0400

    added examples

--- havelock-api/willie-havelock.py
+++ havelock-api/willie-havelock.py
@@ -1,26 +1,23 @@
 import willie
 import havelock
-import re
 
 tickerbot = havelock.Havelock('ticker', 'ticker-key')
 
 @willie.module.commands('havelock')
-
+@willie.module.example('.havelock tickerfull symbol am1')
 def havelock(bot, trigger, **kwargs):
-   
+   """havelock api commands, syntax is .havelock command key1 value1 key2 value2, etc"""
    irc_input  = trigger.group()
-
-
    my_list = irc_input.split()
-   
    
    command = my_list[1]
 
+   
    c = len(my_list)-1
-
    i = 2
    j = 3
    
+   #my ugly list iteration
    while(i<c):
       kwargs[my_list[i]] = my_list[j]
       i = i+1
@@ -34,13 +31,13 @@ def havelock(bot, trigger, **kwargs):
 
 
 @willie.module.commands('tickers')
-
+@willie.module.example('.tickers')
 def tickers(bot, trigger):
+   """lists tickers on havelockinvestments.com"""
    data = tickerbot.tickers
    data = list(data)
    data.sort()
    data = str(data)
 
    bot.say(data)
-
 
